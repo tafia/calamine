94103d5099f149ac9e7a61a7401a3c8cfcf9c6da
Patrick Fernie <patrick.fernie@gmail.com>
2017-01-06 11:34:22 -0500

    updated per PR comments

--- src/errors.rs
+++ src/errors.rs
@@ -8,15 +8,6 @@
 use quick_xml::error::Error as XmlError;
 
 error_chain! {
-//    types {
-//        Error, ErrorKind, ResultExt, Result;
-//    }
-
-//     links {
-//         rustup_dist::Error, rustup_dist::ErrorKind, Dist;
-//         rustup_utils::Error, rustup_utils::ErrorKind, Utils;
-//     }
-
     foreign_links {
         Io(::std::io::Error);
         Zip(::zip::result::ZipError);
@@ -26,13 +17,6 @@ error_chain! {
         Utf8(::std::str::Utf8Error);
         FromUtf8(::std::string::FromUtf8Error);
     }
-
-//     errors {
-//         InvalidToolchainName(t: String) {
-//             description("invalid toolchain name")
-//             display("invalid toolchain name: '{}'", t)
-//         }
-//     }
 }
 
 impl From<(XmlError, usize)> for Error {
