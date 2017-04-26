35c8245aab8ae4bfbc261dfa6e76d71f78123523
Johann Tuffe <tafia973@gmail.com>
2017-04-26 14:42:56 +0800

    treat codepage 21010 as codepage 1200

--- src/utils.rs
+++ src/utils.rs
@@ -63,6 +63,9 @@ pub fn encoding_from_windows_code_page(cp: usize) -> Option<&'static Encoding> {
         1201 => Some(encoding_rs::UTF_16BE),
         1200 => Some(encoding_rs::UTF_16LE),
         949 => Some(encoding_rs::EUC_KR),
+        // This is actually not a valid codepage but it happens with some unknown writer
+        // Looking on internet, it looks like excel treats this as codepage 1200
+        21010 => Some(encoding_rs::UTF_16LE),
         // Not available because not in the Encoding Standard
         //28591 => Some(encoding_rs::ISO_8859_1),
         //38598 => Some(encoding_rs::whatwg::ISO_8859_8_I),
