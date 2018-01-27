9620bdc38477db74cc2c008cb70dd961de1529ff
Johann Tuffe <tafia973@gmail.com>
2018-01-27 23:00:40 +0800

    fix benches

--- benches/basic.rs
+++ benches/basic.rs
@@ -4,85 +4,44 @@ extern crate calamine;
 extern crate test;
 
 use test::Bencher;
-use calamine::Sheets;
+use calamine::{Xls, Xlsx, Xlsb, Ods, Reader, open_workbook};
+use std::io::{BufReader};
 use std::fs::File;
+
+fn count<R: Reader<RS = BufReader<File>>>(path: &str) -> usize {
+    let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
+    let mut excel: R = open_workbook(&path).expect("cannot open excel file");
+
+    let sheets = excel.sheet_names().to_owned();
+    let mut count = 0;
+    for s in sheets {
+        count += excel
+            .worksheet_range(&s)
+            .unwrap()
+            .unwrap()
+            .rows()
+            .flat_map(|r| r.iter())
+            .count();
+    }
+    count
+}
 
 #[bench]
 fn bench_xls(b: &mut Bencher) {
-    b.iter(|| {
-        let path = format!("{}/tests/issues.xls", env!("CARGO_MANIFEST_DIR"));
-        let mut excel = Sheets::<File>::open(&path).expect("cannot open excel file");
-
-        let sheets = excel.sheet_names().unwrap();
-        let mut count = 0;
-        for s in sheets {
-            count += excel
-                .worksheet_range(&s)
-                .unwrap()
-                .rows()
-                .flat_map(|r| r.iter())
-                .count();
-        }
-        count
-    })
+    b.iter(|| count::<Xls<_>>("tests/issues.xls"));
 }
 
 #[bench]
 fn bench_xlsx(b: &mut Bencher) {
-    b.iter(|| {
-        let path = format!("{}/tests/issues.xlsx", env!("CARGO_MANIFEST_DIR"));
-        let mut excel = Sheets::<File>::open(&path).expect("cannot open excel file");
-
-        let sheets = excel.sheet_names().unwrap();
-        let mut count = 0;
-        for s in sheets {
-            count += excel
-                .worksheet_range(&s)
-                .unwrap()
-                .rows()
-                .flat_map(|r| r.iter())
-                .count();
-        }
-        count
-    })
+    b.iter(|| count::<Xlsx<_>>("tests/issues.xlsx"));
 }
 
 #[bench]
 fn bench_xlsb(b: &mut Bencher) {
-    b.iter(|| {
-        let path = format!("{}/tests/issues.xlsb", env!("CARGO_MANIFEST_DIR"));
-        let mut excel = Sheets::<File>::open(&path).expect("cannot open excel file");
-
-        let sheets = excel.sheet_names().unwrap();
-        let mut count = 0;
-        for s in sheets {
-            count += excel
-                .worksheet_range(&s)
-                .unwrap()
-                .rows()
-                .flat_map(|r| r.iter())
-                .count();
-        }
-        count
-    })
+    b.iter(|| count::<Xlsb<_>>("tests/issues.xlsb"));
 }
 
 #[bench]
 fn bench_ods(b: &mut Bencher) {
-    b.iter(|| {
-        let path = format!("{}/tests/issues.ods", env!("CARGO_MANIFEST_DIR"));
-        let mut excel = Sheets::<File>::open(&path).expect("cannot open excel file");
-
-        let sheets = excel.sheet_names().unwrap();
-        let mut count = 0;
-        for s in sheets {
-            count += excel
-                .worksheet_range(&s)
-                .unwrap()
-                .rows()
-                .flat_map(|r| r.iter())
-                .count();
-        }
-        count
-    })
+    b.iter(|| count::<Ods<_>>("tests/issues.ods"));
 }
