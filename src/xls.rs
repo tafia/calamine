58cff0b9ff0b38a264cd441733e690b87aa819f3
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-08-15 08:19:05 -0400

    clippy::match_ref_pats
    warning: you don't need to add `&` to all patterns
        --> src/xls.rs:1304:5
         |
    1304 | /     match r {
    1305 | |         &[0x00, .., 0xFF, 0xFF] => Ok(None), // String, value should be in next record
    1306 | |         &[0x01, _, b, .., 0xFF, 0xFF] => Ok(Some(DataType::Bool(b != 0))),
    1307 | |         &[0x02, _, e, .., 0xFF, 0xFF] => parse_err(e).map(Some),
    ...    |
    1312 | |         _ => Ok(Some(DataType::Float(read_f64(r)))),
    1313 | |     }
         | |_____^
         |
         = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#match_ref_pats
         = note: `#[warn(clippy::match_ref_pats)]` on by default
    help: instead of prefixing all patterns with `&`, you can dereference the expression
         |
    1304 ~     match *r {
    1305 ~         [0x00, .., 0xFF, 0xFF] => Ok(None), // String, value should be in next record
    1306 ~         [0x01, _, b, .., 0xFF, 0xFF] => Ok(Some(DataType::Bool(b != 0))),
    1307 ~         [0x02, _, e, .., 0xFF, 0xFF] => parse_err(e).map(Some),
    1308 ~         [e, .., 0xFF, 0xFF] => Err(XlsError::Unrecognized {

--- src/xls.rs
+++ src/xls.rs
@@ -1301,11 +1301,11 @@ fn parse_formula(
 
 /// FormulaValue [MS-XLS 2.5.133]
 fn parse_formula_value(r: &[u8]) -> Result<Option<DataType>, XlsError> {
-    match r {
-        &[0x00, .., 0xFF, 0xFF] => Ok(None), // String, value should be in next record
-        &[0x01, _, b, .., 0xFF, 0xFF] => Ok(Some(DataType::Bool(b != 0))),
-        &[0x02, _, e, .., 0xFF, 0xFF] => parse_err(e).map(Some),
-        &[e, .., 0xFF, 0xFF] => Err(XlsError::Unrecognized {
+    match *r {
+        [0x00, .., 0xFF, 0xFF] => Ok(None), // String, value should be in next record
+        [0x01, _, b, .., 0xFF, 0xFF] => Ok(Some(DataType::Bool(b != 0))),
+        [0x02, _, e, .., 0xFF, 0xFF] => parse_err(e).map(Some),
+        [e, .., 0xFF, 0xFF] => Err(XlsError::Unrecognized {
             typ: "error",
             val: e,
         }),
