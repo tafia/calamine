cb9be021598ff3e94ebf0caeaab9a01bbafe284d
John McNamara <jmcnamara@cpan.org>
2025-07-17 11:11:14 +0100

    docs: add documentation to the example files
    Add usage and documentation to the example files and also refactor
    some of the code and variable names to make the intent clearer.

--- examples/excel_to_csv.rs
+++ examples/excel_to_csv.rs
@@ -2,57 +2,82 @@
 //
 // Copyright 2016-2025, Johann Tuffe.
 
-use calamine::{open_workbook_auto, Data, Range, Reader};
+//! An example for using the `calamine` crate to convert an Excel file to CSV.
+//!
+//! Converts XLSX, XLSM, XLSB, and XLS files. The filename and sheet name must
+//! be specified as command line arguments. The output CSV will be written to a
+//! file with the same name as the input file, but with a `.csv` extension.
+
 use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
-fn main() {
-    // converts first argument into a csv (same name, silently overrides
-    // if the file already exists
+use calamine::{open_workbook_auto, Data, Range, Reader};
 
-    let file = env::args()
+// usage: cargo run --example excel_to_csv file.xls[xmb] sheet_name
+//
+// Where:
+// - `file.xls[xmb]` is the Excel file to convert. Required.
+// - `sheet_name` is the name of the sheet to convert. Required.
+//
+// The output will be written to a file with the same name as the input file,
+// including the path, but with a `.csv` extension.
+//
+fn main() {
+    let excel_file = env::args()
         .nth(1)
         .expect("Please provide an excel file to convert");
-    let sheet = env::args()
+
+    let sheet_name = env::args()
         .nth(2)
         .expect("Expecting a sheet name as second argument");
 
-    let sce = PathBuf::from(file);
-    match sce.extension().and_then(|s| s.to_str()) {
+    let excel_path = PathBuf::from(excel_file);
+    match excel_path.extension().and_then(|s| s.to_str()) {
         Some("xlsx") | Some("xlsm") | Some("xlsb") | Some("xls") => (),
         _ => panic!("Expecting an excel file"),
     }
 
-    let dest = sce.with_extension("csv");
-    let mut dest = BufWriter::new(File::create(dest).unwrap());
-    let mut xl = open_workbook_auto(&sce).unwrap();
-    let range = xl.worksheet_range(&sheet).unwrap();
+    let csv_path = excel_path.with_extension("csv");
+    let mut csv_file = BufWriter::new(File::create(csv_path).unwrap());
+    let mut workbook = open_workbook_auto(&excel_path).unwrap();
+    let range = workbook.worksheet_range(&sheet_name).unwrap();
 
-    write_range(&mut dest, &range).unwrap();
+    write_to_csv(&mut csv_file, &range).unwrap();
 }
 
-fn write_range<W: Write>(dest: &mut W, range: &Range<Data>) -> std::io::Result<()> {
-    let n = range.get_size().1 - 1;
-    for r in range.rows() {
-        for (i, c) in r.iter().enumerate() {
-            match *c {
+// Write the Excel data as strings to a CSV file. Uses a semicolon (`;`) as the
+// field separator.
+//
+// Note, this is a simplified version of CSV and doesn't handle quoting of
+// separators or other special cases. See the `csv.rs` crate for a more robust
+// solution.
+fn write_to_csv<W: Write>(output_file: &mut W, range: &Range<Data>) -> std::io::Result<()> {
+    let max_column = range.get_size().1 - 1;
+
+    for rows in range.rows() {
+        for (col_number, cell_data) in rows.iter().enumerate() {
+            match *cell_data {
                 Data::Empty => Ok(()),
+                Data::Int(ref i) => write!(output_file, "{i}"),
+                Data::Bool(ref b) => write!(output_file, "{b}"),
+                Data::Error(ref e) => write!(output_file, "{e:?}"),
+                Data::Float(ref f) => write!(output_file, "{f}"),
+                Data::DateTime(ref d) => write!(output_file, "{}", d.as_f64()),
                 Data::String(ref s) | Data::DateTimeIso(ref s) | Data::DurationIso(ref s) => {
-                    write!(dest, "{s}")
+                    write!(output_file, "{s}")
                 }
-                Data::Float(ref f) => write!(dest, "{f}"),
-                Data::DateTime(ref d) => write!(dest, "{}", d.as_f64()),
-                Data::Int(ref i) => write!(dest, "{i}"),
-                Data::Error(ref e) => write!(dest, "{e:?}"),
-                Data::Bool(ref b) => write!(dest, "{b}"),
             }?;
-            if i != n {
-                write!(dest, ";")?;
+
+            // Write the field separator except for the last column.
+            if col_number != max_column {
+                write!(output_file, ";")?;
             }
         }
-        write!(dest, "\r\n")?;
+
+        write!(output_file, "\r\n")?;
     }
+
     Ok(())
 }
