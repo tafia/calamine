91ecdf2aed12b15abb0e60b3a0c2d0d0eea4e4a5
siqpush <98565842+siqpush@users.noreply.github.com>
2025-09-20 14:48:34 -0700

    Rebase, cargo fmt+clippy and public facing api adjustments

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,7 @@
 
 #[macro_use]
 mod utils;
+
 mod auto;
 mod cfb;
 mod datatype;
@@ -374,9 +375,9 @@ where
     #[cfg(feature = "picture")]
     #[cfg_attr(docsrs, doc(cfg(feature = "picture")))]
     fn pictures(&self) -> Option<Vec<(String, Vec<u8>)>>;
-    
-    #[cfg(feature = "pivot-cache")]
-    fn pivot_tables(&self) -> Option<Vec<String>>;
+    //
+    // #[cfg(feature = "pivot-cache")]
+    // fn pivot_tables(&self) -> Option<Vec<String>>;
 }
 
 /// A trait to share spreadsheets reader functions across different `FileType`s
