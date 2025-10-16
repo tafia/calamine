86624d7b924ecf539dd1c8486a7aeec2ceb439af
joel gilmore <98565842+siqpush@users.noreply.github.com>
2025-10-15 17:16:05 -0700

    line spacing

--- src/xlsx/mod.rs
+++ src/xlsx/mod.rs
@@ -1798,6 +1798,7 @@ impl<RS: Read + Seek> Reader<RS> for Xlsx<RS> {
         xlsx.read_pictures()?;
         #[cfg(feature = "pivot-cache")]
         xlsx.read_pivot_table_metadata()?;
+
         Ok(xlsx)
     }
 
