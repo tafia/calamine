d532998a480ea32a7c610e354b9cafea1e8e271b
Joel Mislav Kunst <7sedam7@gmail.com>
2026-02-13 12:38:20 +0200

    Filter chunks shorter then 6 bytes that couse panic

--- src/xls.rs
+++ src/xls.rs
@@ -384,11 +384,17 @@ impl<RS: Read + Seek> Xls<RS> {
                     0x0017 => {
                         // ExternSheet
                         let cxti = read_u16(r.data) as usize;
-                        xtis.extend(r.data[2..].chunks(6).take(cxti).map(|xti| Xti {
-                            _isup_book: read_u16(&xti[..2]),
-                            itab_first: read_i16(&xti[2..4]),
-                            _itab_last: read_i16(&xti[4..]),
-                        }));
+                        xtis.extend(
+                            r.data[2..]
+                                .chunks(6)
+                                .take(cxti)
+                                .filter(|xti| xti.len() >= 6)
+                                .map(|xti| Xti {
+                                    _isup_book: read_u16(&xti[..2]),
+                                    itab_first: read_i16(&xti[2..4]),
+                                    _itab_last: read_i16(&xti[4..]),
+                                }),
+                        );
                     }
                     0x00FC => strings = parse_sst(&mut r, &encoding)?, // SST
                     #[cfg(feature = "picture")]
