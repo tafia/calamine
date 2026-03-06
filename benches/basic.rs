a5fe10cb45c8c41acc34766e091fc6c7b6be395f
John McNamara <jmcnamara@cpan.org>
2026-03-06 19:26:57 +0000

    bench: change to criterion for benchmark testing
    Change to the `criterion.rs` crate for `bench` testing. This avoids
    the need for +nightly to run the benchmarks.

--- benches/basic.rs
+++ benches/basic.rs
@@ -2,14 +2,15 @@
 //
 // Copyright 2016-2025, Johann Tuffe.
 
-#![feature(test)]
-
-extern crate test;
+// Simple benchmarks to check for performance regressions.
+//
+// Run with: `cargo bench`
+// HTML reports are written to target/criterion/.
 
 use calamine::{open_workbook, Ods, Reader, Xls, Xlsb, Xlsx};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs::File;
 use std::io::BufReader;
-use test::Bencher;
 
 fn count<R: Reader<BufReader<File>>>(path: &str) -> usize {
     let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
@@ -28,60 +29,71 @@ fn count<R: Reader<BufReader<File>>>(path: &str) -> usize {
     count
 }
 
-#[bench]
-fn bench_xls(b: &mut Bencher) {
-    b.iter(|| count::<Xls<_>>("tests/issues.xls"));
-}
+fn count_cells_reader_xlsx(path: &str) -> usize {
+    let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
+    let mut excel: Xlsx<_> = open_workbook(&path).expect("cannot open excel file");
 
-#[bench]
-fn bench_xlsx(b: &mut Bencher) {
-    b.iter(|| count::<Xlsx<_>>("tests/issues.xlsx"));
-}
-
-#[bench]
-fn bench_xlsb(b: &mut Bencher) {
-    b.iter(|| count::<Xlsb<_>>("tests/issues.xlsb"));
-}
-
-#[bench]
-fn bench_ods(b: &mut Bencher) {
-    b.iter(|| count::<Ods<_>>("tests/issues.ods"));
-}
-
-#[bench]
-fn bench_xlsx_cells_reader(b: &mut Bencher) {
-    fn count<R: Reader<BufReader<File>>>(path: &str) -> usize {
-        let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
-        let mut excel: Xlsx<_> = open_workbook(&path).expect("cannot open excel file");
-
-        let sheets = excel.sheet_names();
-        let mut count = 0;
-        for s in sheets {
-            let mut cells_reader = excel.worksheet_cells_reader(&s).unwrap();
-            while cells_reader.next_cell().unwrap().is_some() {
-                count += 1;
-            }
+    let sheets = excel.sheet_names();
+    let mut count = 0;
+    for s in sheets {
+        let mut cells_reader = excel.worksheet_cells_reader(&s).unwrap();
+        while cells_reader.next_cell().unwrap().is_some() {
+            count += 1;
         }
-        count
     }
-    b.iter(|| count::<Xlsx<_>>("tests/issues.xlsx"));
+    count
 }
 
-#[bench]
-fn bench_xlsb_cells_reader(b: &mut Bencher) {
-    fn count<R: Reader<BufReader<File>>>(path: &str) -> usize {
-        let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
-        let mut excel: Xlsb<_> = open_workbook(&path).expect("cannot open excel file");
+fn count_cells_reader_xlsb(path: &str) -> usize {
+    let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path);
+    let mut excel: Xlsb<_> = open_workbook(&path).expect("cannot open excel file");
 
-        let sheets = excel.sheet_names();
-        let mut count = 0;
-        for s in sheets {
-            let mut cells_reader = excel.worksheet_cells_reader(&s).unwrap();
-            while cells_reader.next_cell().unwrap().is_some() {
-                count += 1;
-            }
+    let sheets = excel.sheet_names();
+    let mut count = 0;
+    for s in sheets {
+        let mut cells_reader = excel.worksheet_cells_reader(&s).unwrap();
+        while cells_reader.next_cell().unwrap().is_some() {
+            count += 1;
         }
-        count
     }
-    b.iter(|| count::<Xlsx<_>>("tests/issues.xlsb"));
+    count
 }
+
+fn bench_xls(c: &mut Criterion) {
+    c.bench_function("xls", |b| b.iter(|| count::<Xls<_>>("tests/issues.xls")));
+}
+
+fn bench_xlsx(c: &mut Criterion) {
+    c.bench_function("xlsx", |b| b.iter(|| count::<Xlsx<_>>("tests/issues.xlsx")));
+}
+
+fn bench_xlsb(c: &mut Criterion) {
+    c.bench_function("xlsb", |b| b.iter(|| count::<Xlsb<_>>("tests/issues.xlsb")));
+}
+
+fn bench_ods(c: &mut Criterion) {
+    c.bench_function("ods", |b| b.iter(|| count::<Ods<_>>("tests/issues.ods")));
+}
+
+fn bench_xlsx_cells_reader(c: &mut Criterion) {
+    c.bench_function("xlsx_cells_reader", |b| {
+        b.iter(|| count_cells_reader_xlsx("tests/issues.xlsx"))
+    });
+}
+
+fn bench_xlsb_cells_reader(c: &mut Criterion) {
+    c.bench_function("xlsb_cells_reader", |b| {
+        b.iter(|| count_cells_reader_xlsb("tests/issues.xlsb"))
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_xls,
+    bench_xlsx,
+    bench_xlsb,
+    bench_ods,
+    bench_xlsx_cells_reader,
+    bench_xlsb_cells_reader
+);
+criterion_main!(benches);
