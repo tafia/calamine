d206c9bdc94fbee5c00b1a8e4561c5306ea8a343
sftse <c@farsight.net>
2026-07-02 10:43:14 +0200

    fix(xls): better error message when file too small (#670)
    Give a better error message if the xls file is too small.

--- tests/test.rs
+++ tests/test.rs
@@ -3473,3 +3473,17 @@ fn test_hyperlinks_xlsx() {
         Err(calamine::XlsxError::WorksheetNotFound(_))
     ));
 }
+
+#[test]
+fn too_small_xls() {
+    let path = test_path("too_small.xls");
+    let res: Result<Xls<_>, _> = open_workbook(&path);
+    match res {
+
+        Err(calamine::XlsError::Cfb(e)) if e.to_string() == "Invalid OLE (68 bytes is too small, signature \\x41\\x74\\x74\\x61\\x63\\x68\\x6d\\x65 invalid, not an office document?)" => {}
+        Err(calamine::XlsError::Cfb(e)) => {
+            panic!("Is expected to return CfbError::Ole(_) error: {e}")
+        }
+        Ok(_) | Err(_) => panic!("Is expected to return CfbError::Ole(_) error"),
+    }
+}
